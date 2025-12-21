0c14f041a64cdc600d6a16dc97be1353e38fe524
applleeee <warm9806@gmail.com>
2025-12-21 20:04:52 +0900

    Feat : GA, 뷰저블 스크립트 삽입

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,40 @@ export default function RootLayout({
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false&libraries=services`}
           strategy="afterInteractive"
         />
+
+        {/* Analytics Scripts - 프로덕션 환경에서만 로드 */}
+        {process.env.NODE_ENV === "production" && (
+          <>
+            {/* Google Analytics */}
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-YNXLDLYEEE"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-YNXLDLYEEE');
+              `}
+            </Script>
+
+            {/* 뷰저블(Beusable) */}
+            <Script id="beusable" strategy="afterInteractive">
+              {`
+                (function(w, d, a){
+                  w.__beusablerumclient__ = {
+                    load : function(src){
+                      var b = d.createElement("script");
+                      b.src = src; b.async=true; b.type = "text/javascript";
+                      d.getElementsByTagName("head")[0].appendChild(b);
+                    }
+                  };w.__beusablerumclient__.load(a + "?url=" + encodeURIComponent(d.URL));
+                })(window, document, "//rum.beusable.net/load/b251219e074853u702");
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
